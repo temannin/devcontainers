b5abae75be98f791f06cffad9447c23549105f5e
temannin <tyler@tylermanning.org>
2025-09-27 18:25:51 -0400

    Fix build script variable checks to ensure proper handling of CI environment variables by enclosing them in quotes.

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # if CI is defined, exit with an error
-if [ $CI != "" ]; then
+if [ "$CI" != "" ]; then
     echo "This script should not be run in a CI environment. Exiting."
     exit 1
 fi
 
 # if GITHUB_ACTIONS is defined, exit with an error
-if [ $GITHUB_ACTIONS != "" ]; then
+if [ "$GITHUB_ACTIONS" != "" ]; then
     echo "This script should not be run in a GitHub Actions environment. Exiting."
     exit 1
 fi
 
 # if GITHUB_RUN_ID is defined, exit with an error
-if [ $GITHUB_RUN_ID != "" ]; then
+if [ "$GITHUB_RUN_ID" != "" ]; then
     echo "This script should not be run in a GitHub Actions environment. Exiting."
     exit 1
 fi
