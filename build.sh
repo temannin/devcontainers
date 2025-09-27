fcf73db5efa95a9d81ee1458635b2ebdffcac20f
temannin <tyler@tylermanning.org>
2025-09-27 17:09:10 -0400

    Add SSH key generation and server configuration to Docker setup; update build script to pass SSH public key as build argument and expose SSH port.

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@
 # take in the image name as an argument
 IMAGE_NAME=$1
 
+# create a SSH pub/priv key pair where it will go to ~/.ssh/id_rsa.pub
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+
 # build the image in directory $IMAGE_NAME
-docker build -t $IMAGE_NAME . -f $IMAGE_NAME/Dockerfile
+docker build -t $IMAGE_NAME --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)" -f $IMAGE_NAME/Dockerfile .
 
 # if build is successful, run the image
 if [ $? -eq 0 ]; then
-    docker run -e GITHUB_TOKEN=$(gh auth token) --rm -it $IMAGE_NAME
+    docker run -e GITHUB_TOKEN=$(gh auth token) -p 2222:22 --rm -it $IMAGE_NAME
 fi
 
