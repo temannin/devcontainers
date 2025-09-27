cf47ac88bbd85f09d9529251a2aab5f8f3d20727
temannin <tyler@tylermanning.org>
2025-09-27 18:33:36 -0400

    Update build.sh to enhance usage instructions and allow passing additional arguments to docker run command

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Usage: ./build.sh <image_name> [docker_run_args...]
+# Examples: 
+#   ./build.sh myapp
+#   ./build.sh myapp -v .:/app
+#   ./build.sh myapp -v .:/app -p 8080:8080 --name mycontainer
+
 # if CI is defined, exit with an error
 if [ "$CI" != "" ]; then
     echo "This script should not be run in a CI environment. Exiting."
@@ -21,6 +27,10 @@ fi
 # take in the image name as an argument
 IMAGE_NAME=$1
 
+# capture all remaining arguments to pass to docker run
+shift  # remove the first argument (image name)
+DOCKER_RUN_ARGS="$@"
+
 # create a SSH pub/priv key pair where it will go to ~/.ssh/id_rsa.pub
 # auto overwrite the existing key if it exists
 if [ -f ~/.ssh/$IMAGE_NAME/devcontainer_id_rsa.pub ]; then
@@ -34,6 +44,6 @@ docker build -t $IMAGE_NAME --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/$IMAGE_NAME
 
 # if build is successful, run the image
 if [ $? -eq 0 ]; then
-    docker run -e GITHUB_TOKEN=$(gh auth token) -p 2222:22 --rm -it $IMAGE_NAME
+    docker run -e GITHUB_TOKEN=$(gh auth token) -p 2222:22 --rm -it $DOCKER_RUN_ARGS $IMAGE_NAME
 fi
 
