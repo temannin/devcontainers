571618be68f1bf82edd5c90c979e1df078667c68
tylermanning <tylerericmanning@gmail.com>
2025-10-09 09:55:29 -0400

    Implement lazydocker installation script with architecture detection and binary download functionality.

--- features/lazydocker/install.sh
+++ features/lazydocker/install.sh
@@ -1,1 +1,27 @@
-echo "TESTING DEVCONTAINERS FEATURES"
+#!/bin/bash
+# source from: https://github.com/jesseduffield/lazydocker/blob/master/scripts/install_update_linux.sh
+
+apt-get update && apt-get install -y curl
+
+# allow specifying different destination directory
+DIR="${DIR:-"/usr/local/bin"}"
+
+# map different architecture variations to the available binaries
+ARCH=$(uname -m)
+case $ARCH in
+    i386|i686) ARCH=x86 ;;
+    armv6*) ARCH=armv6 ;;
+    armv7*) ARCH=armv7 ;;
+    aarch64*) ARCH=arm64 ;;
+esac
+
+# prepare the download URL
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/jesseduffield/lazydocker/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="lazydocker_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_${ARCH}.tar.gz"
+GITHUB_URL="https://github.com/jesseduffield/lazydocker/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+# install/update the local binary
+curl -L -o lazydocker.tar.gz $GITHUB_URL
+tar xzvf lazydocker.tar.gz lazydocker --no-same-owner
+install -Dm 755 lazydocker -t "$DIR"
+rm lazydocker lazydocker.tar.gz
